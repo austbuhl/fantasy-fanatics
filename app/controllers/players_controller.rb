20e72fc37a30467415df33f93e4beed01defd32f
Sean LaFlam <sean.laflam@gmail.com>
2020-09-29 09:11:37 -0400

    Built out Player Table

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,9 +1,22 @@
 class PlayersController < ApplicationController
   def index
-    @players = Player.all
+    if params[:sort]
+      @players = Player.order(params[:sort])
+    elsif params[:search]
+      search_players
+    else
+      @players = Player.all
+    end
+  end
+
+  def search_players
+    if @player = Player.all.find{|player| player.name.include?(params[:search])}
+      redirect_to player_path(@player)
+    end
   end
 
   def show
     @player = Player.find(params[:id])
   end
+
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,14 @@ class Player < ApplicationRecord
   has_many :player_teams
   has_many :teams, through: :player_teams
   has_many :rankings
+
+  def avg_ranking
+    ranking = 0
+    avg = 0
+    self.rankings.each do |rank|
+      ranking += rank.ranking
+      avg = 1.0*ranking/self.rankings.count
+    end
+    avg.round(2)
+  end
 end
